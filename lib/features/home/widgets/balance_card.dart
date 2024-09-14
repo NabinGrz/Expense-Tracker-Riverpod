362f624103e6ef460ada94f4728f43b94d2ada84
Nabin Gurung <nabin.gurung@vedastudios.com>
2024-09-14 13:55:35 +0545

    feat: add animation

--- lib/features/home/widgets/balance_card.dart
+++ lib/features/home/widgets/balance_card.dart
@@ -28,182 +28,213 @@ class BalanceCard extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _BalanceCardState();
 }
 
-class _BalanceCardState extends ConsumerState<BalanceCard> {
+class _BalanceCardState extends ConsumerState<BalanceCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    final curvedAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.fastOutSlowIn,
+    );
+    animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+    super.initState();
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-      width: double.infinity,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: AppColor.primary,
-        borderRadius: BorderRadius.circular(20),
-        image: const DecorationImage(
-            image: sp.Svg("assets/images/header_background.svg")),
-        boxShadow: [
-          BoxShadow(
-            color: const Color(0xff849D9B).withOpacity(0.78),
-            spreadRadius: -7,
-            offset: const Offset(0, 10),
-            blurRadius: 13.4,
-          )
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Remaining Balance",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.white,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          10.hGap,
-          StreamBuilder(
-              stream: FirebaseQueryHelper.getSingleDocumentAsStream(
-                  collectionPath: "balance", docID: "G0sKt8y5dvwNsTv63m2f"),
-              builder: (context, snapshot) {
-                final balance = snapshot.data?.data();
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Cash",
-                          style: TextStyle(
-                            color: Color(0xffD0E5E4),
-                          ),
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Rs ${int.tryParse(balance?['cash'] ?? "").toCurrency}",
-                              style: const TextStyle(
-                                fontSize: 22,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            4.wGap,
-                            InkWell(
-                                onTap: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return BalanceUpdateDialog(
-                                        isCash: true,
-                                        docId: "G0sKt8y5dvwNsTv63m2f",
-                                        cashAmount: balance?['cash'],
-                                        bankAmount: balance?['bank'],
-                                      );
-                                    },
-                                  );
-                                },
-                                child: const Icon(
-                                  Icons.edit,
-                                  color: Colors.white,
-                                ))
-                          ],
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Bank",
-                          style: TextStyle(
-                            color: Color(0xffD0E5E4),
-                          ),
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Rs ${int.tryParse(balance?['bank'] ?? "").toCurrency}",
-                              style: const TextStyle(
-                                fontSize: 22,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            4.wGap,
-                            InkWell(
-                                onTap: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return BalanceUpdateDialog(
-                                        isCash: false,
-                                        docId: "G0sKt8y5dvwNsTv63m2f",
-                                        cashAmount: balance?['cash'],
-                                        bankAmount: balance?['bank'],
-                                      );
-                                    },
-                                  );
-                                },
-                                child: const Icon(
-                                  Icons.edit,
-                                  color: Colors.white,
-                                ))
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                );
-              }),
-          25.hGap,
-          Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white.withOpacity(0.15),
-                ),
-                child: SvgPicture.asset("assets/images/up_arrow.svg"),
-              ),
-              8.wGap,
-              const Text(
-                "Today's Total Expense",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Color(0xffD0E5E4),
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+    return ScaleTransition(
+      scale: animation,
+      child: FadeTransition(
+        opacity: animation,
+        child: Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+          width: double.infinity,
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: AppColor.primary,
+            borderRadius: BorderRadius.circular(20),
+            image: const DecorationImage(
+                image: sp.Svg("assets/images/header_background.svg")),
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xff849D9B).withOpacity(0.78),
+                spreadRadius: -7,
+                offset: const Offset(0, 10),
+                blurRadius: 13.4,
+              )
             ],
           ),
-          8.hGap,
-          StreamBuilder(
-              stream: widget.sortedExpenseSubject,
-              builder: (context, snapshot) {
-                List<Expense>? expenses = snapshot.data?.where(
-                  (element) {
-                    final expenseDate = DateTime.parse(element.createAt);
-                    return expenseDate.isSameDateAs(DateTime.now());
-                  },
-                ).toList();
-                return Text(
-                  "Rs ${expenses?.map(
-                        (e) => e.amount,
-                      ).sum().toCurrency}",
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Remaining Balance",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              10.hGap,
+              StreamBuilder(
+                  stream: FirebaseQueryHelper.getSingleDocumentAsStream(
+                      collectionPath: "balance", docID: "G0sKt8y5dvwNsTv63m2f"),
+                  builder: (context, snapshot) {
+                    final balance = snapshot.data?.data();
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Cash",
+                              style: TextStyle(
+                                color: Color(0xffD0E5E4),
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Rs ${int.tryParse(balance?['cash'] ?? "").toCurrency}",
+                                  style: const TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                4.wGap,
+                                InkWell(
+                                    onTap: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return BalanceUpdateDialog(
+                                            isCash: true,
+                                            docId: "G0sKt8y5dvwNsTv63m2f",
+                                            cashAmount: balance?['cash'],
+                                            bankAmount: balance?['bank'],
+                                          );
+                                        },
+                                      );
+                                    },
+                                    child: const Icon(
+                                      Icons.edit,
+                                      color: Colors.white,
+                                    ))
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Bank",
+                              style: TextStyle(
+                                color: Color(0xffD0E5E4),
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Rs ${int.tryParse(balance?['bank'] ?? "").toCurrency}",
+                                  style: const TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                4.wGap,
+                                InkWell(
+                                    onTap: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (context) {
+                                          return BalanceUpdateDialog(
+                                            isCash: false,
+                                            docId: "G0sKt8y5dvwNsTv63m2f",
+                                            cashAmount: balance?['cash'],
+                                            bankAmount: balance?['bank'],
+                                          );
+                                        },
+                                      );
+                                    },
+                                    child: const Icon(
+                                      Icons.edit,
+                                      color: Colors.white,
+                                    ))
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    );
+                  }),
+              25.hGap,
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.15),
+                    ),
+                    child: SvgPicture.asset("assets/images/up_arrow.svg"),
                   ),
-                );
-              }),
-        ],
+                  8.wGap,
+                  const Text(
+                    "Today's Total Expense",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xffD0E5E4),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              8.hGap,
+              StreamBuilder(
+                  stream: widget.sortedExpenseSubject,
+                  builder: (context, snapshot) {
+                    List<Expense>? expenses = snapshot.data?.where(
+                      (element) {
+                        final expenseDate = DateTime.parse(element.createAt);
+                        return expenseDate.isSameDateAs(DateTime.now());
+                      },
+                    ).toList();
+                    return Text(
+                      "Rs ${expenses?.map(
+                            (e) => e.amount,
+                          ).sum().toCurrency}",
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  }),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/features/home/widgets/date_filter_row.dart
+++ lib/features/home/widgets/date_filter_row.dart
@@ -6,25 +6,63 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../constants/app_color.dart';
 import '../../../shared/provider/sort_by_provider.dart';
 
-class DateFilterRow extends ConsumerWidget {
-  const DateFilterRow({
-    super.key,
-  });
+class DateFilterRow extends ConsumerStatefulWidget {
+  const DateFilterRow({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xffF4F6F6),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          filterBox("Today", selectedDateFilter: DateFilter.today),
-          filterBox("Yesterday", selectedDateFilter: DateFilter.yesterday),
-          filterBox("2 Weeks", selectedDateFilter: DateFilter.twoweeks),
-          filterBox("Monthly", selectedDateFilter: DateFilter.monthly),
-        ],
+  ConsumerState<ConsumerStatefulWidget> createState() => _DateFilterRowState();
+}
+
+class _DateFilterRowState extends ConsumerState<DateFilterRow>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animation;
+  late Animation<double> opacityAnimation;
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    final curvedAnimation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.fastOutSlowIn,
+    );
+    animation =
+        Tween<Offset>(begin: const Offset(0, -1), end: const Offset(0, 0))
+            .animate(curvedAnimation);
+    opacityAnimation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+    super.initState();
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: animation,
+      child: FadeTransition(
+        opacity: opacityAnimation,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xffF4F6F6),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              filterBox("Today", selectedDateFilter: DateFilter.today),
+              filterBox("Yesterday", selectedDateFilter: DateFilter.yesterday),
+              filterBox("2 Weeks", selectedDateFilter: DateFilter.twoweeks),
+              filterBox("Monthly", selectedDateFilter: DateFilter.monthly),
+            ],
+          ),
+        ),
       ),
     );
   }
